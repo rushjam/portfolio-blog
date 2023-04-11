5df17432edc3ee8f5c6a4db8b8ff87c1a54274e8
Ramani <rushikramani@csu.fullerton.edu>
2023-04-11 12:33:25 -0700

    Color shceme changed

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -7,10 +7,10 @@ const headerNavLinks = [
   //   href: '/tags',
   //   title: 'Tags',
   // },
-  {
-    href: '/projects',
-    title: 'Projects',
-  },
+  // {
+  //   href: '/projects',
+  //   title: 'Projects',
+  // },
   {
     href: '/about',
     title: 'About',
