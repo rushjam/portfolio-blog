e572b3b85d3359f1bc6c71d2aa34029b3579bd19
Ramani <rushikramani@csu.fullerton.edu>
2023-04-12 21:48:54 -0700

    Vercel analytics added

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,14 @@
 import '@/css/tailwind.css'
 import '@/css/prism.css'
 import 'katex/dist/katex.css'
+import { Analytics } from '@vercel/analytics/react'
 // import '@/css/docsearch.css' // Uncomment if using algolia docsearch
 // import '@docsearch/css' // Uncomment if using algolia docsearch
 
 import { ThemeProvider } from 'next-themes'
 import Head from 'next/head'
 import siteMetadata from '@/data/siteMetadata'
-import { Analytics } from 'pliny/analytics'
+// import { Analytics } from 'pliny/analytics'
 import { SearchProvider } from 'pliny/search'
 import LayoutWrapper from '@/components/LayoutWrapper'
 export default function App({ Component, pageProps }) {
@@ -16,7 +17,8 @@ export default function App({ Component, pageProps }) {
       <Head>
         <meta content="width=device-width, initial-scale=1" name="viewport" />
       </Head>
-      <Analytics analyticsConfig={siteMetadata.analytics} />
+      {/* <Analytics analyticsConfig={siteMetadata.analytics} /> */}
+      <Analytics />
       <LayoutWrapper>
         <SearchProvider searchConfig={siteMetadata.search}>
           <Component {...pageProps} />
